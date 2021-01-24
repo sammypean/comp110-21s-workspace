8d45a318d8401cd09de12c6477241caa1d82762a
Samantha Pean <sampean@ad.unc.edu>
2021-01-24 13:22:03 -0500

    Had to add a period

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,4 +1,4 @@
-"""My first program for COMP110"""
+"""My first program for COMP110."""
 
 __author__="730408503"
 print("Hello, my name is Samantha. I am ready to enter into the programming world.")
